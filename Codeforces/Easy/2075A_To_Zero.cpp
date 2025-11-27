eb61bf0cabebc901ecee6a66d4b064193d5cd3c7
Cyprian <bieglywybuch@gmail.com>
2025-11-27 20:05:45 +0100

    Solution to 2075A_To_Zero

--- Codeforces/Easy/2075A_To_Zero.cpp
+++ Codeforces/Easy/2075A_To_Zero.cpp
@@ -6,36 +6,28 @@
 
 int main(){
     int t;
-    int n, k;
-    int max_odd, max_even, opperation_count;
+    long long n, k;
+    long long max_odd, max_even;
     std::cin >> t;
     for(int i = 1; i <= t; i++)
     {
+        long long opperation_count = 0;
         std::cin >> n >> k;
-        if(k % 2 == 0)
+        int max_odd = k; int max_even = k-1;
+        if (!(n % 2 == 0))
         {
-            max_even = k;
-            max_odd = k-1;
+            n -= max_odd;
+            opperation_count++;
         }
-        else
+        if(n > 0)
         {
-            max_odd = k;
-            max_even = k-1;
-        }
-        while(!(n == 0))
-        {
-            if(n % 2 == 0)
+            opperation_count += n / max_even;
+            if(n % max_even != 0)
             {
-                n -= max_even;
                 opperation_count++;
-            }
-            else
-            {
-                n -= max_odd;
-                opperation_count++;
-            }
-        }
-        std::cout << opperation_count;
+            }   
+        } 
+        std::cout << opperation_count << "\n";
     }
     return 0;
 }
